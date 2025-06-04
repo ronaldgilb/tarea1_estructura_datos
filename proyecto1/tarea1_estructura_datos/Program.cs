8e75f6abb7de2283c43b0e72c6ed680befc4eafb
ronaldgilb <ronaldgilbreton29@gmail.com>
2025-06-03 22:58:55 -0400

    Add files via upload

---
+++ proyecto1/tarea1_estructura_datos/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = new int[10];
+
+
+
+
+
+        Console.WriteLine("Ingrese numeros hasta el 10:");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Numero {i + 1}: ");
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+
+        
+
+
+
+        int[] ascendente = numeros.OrderBy(n => n).ToArray();
+        Console.WriteLine("\nNumeros  ascendentes:");
+        foreach (int n in ascendente)
+        {
+            Console.Write(n + " ");
+        }
+
+
+
+
+        int[] descendente = numeros.OrderByDescending(n => n).ToArray();
+        Console.WriteLine("\n\nNumeros  descendentes:");
+        foreach (int n in descendente)
+        {
+            Console.Write(n + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
